39bb2ec1ba9cb238a043301537d5ec1e59475692
Susane <147738127+Su-Zhou@users.noreply.github.com>
2024-03-21 19:45:50 -0600

    docs: content video page, accordion, cards and footer

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+console.log('Hello, World!');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const date = new Date().getFullYear();
+    document.getElementById("year").innerHTML = date;
+  });
